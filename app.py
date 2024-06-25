3f6e821de62fd6594445eb891f61568fdd2095bc
Uberlei Brito <uberleidasilva@gmail.com>
2024-06-25 10:43:23 -0300

    aula3

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-print('''
+import os
+
+# def é função
+def exibir_nome_do_programa():
+    print('''
 
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -8,14 +12,38 @@ print('''
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
 ''')
 
-print('1. Cadastrar restaurante')
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
-print('2. Listar restaurante')
+def finalizar_app():
+    os.system('cls')
+    print('4') 
 
-print('3. Ativar restaurante')
+def ecolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
 
-print('4. Sair\n')
+    # PARSE
+    # opcao_escolhida = int(opcao_escolhida)
+    # DESCOBRIR TIPO
+    # print(type(opcao_escolhida))
+    # print(type(1))
 
-opcao_escolhida = input('Escolha uma opção: ')
+    if opcao_escolhida == 1:
+        print('1')
+    elif opcao_escolhida == 2:
+        print('2')
+    elif opcao_escolhida == 3:
+        print('3')
+    else:
+        finalizar_app()
 
-print(f'Você escolheu a opção {opcao_escolhida}')
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    ecolher_opcao()
+
+if __name__ == '__main__':
+    main()
